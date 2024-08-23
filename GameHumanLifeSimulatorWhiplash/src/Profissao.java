d514eb9d6f59031da29b06a9e18d51b8e78ad4fe
Juan <juanpereiracosta@gmail.com>
2024-08-23 11:27:34 +0100

    Atualizações

--- GameHumanLifeSimulatorWhiplash/src/Profissao.java
+++
@@ -1,8 +0,0 @@
-public class Profissao {
-
-    private String nome;
-    private double salarioDia;
-    private boolean formal;
-    private int estatuto;
-    private int nivelMinimoEducacao;
-}
--- GameHumanLifeSimulatorWhiplash/src/Profissao/Profissao.java
+++ GameHumanLifeSimulatorWhiplash/src/Profissao/Profissao.java
@@ -1,4 +1,10 @@
 package Profissao;
 
 public class Profissao {
+
+    private String nome;
+    private double salarioDia;
+    private boolean formal;
+    private int estatuto;
+    private int nivelMinimoEducacao;
 }
